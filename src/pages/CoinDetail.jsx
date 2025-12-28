f068d46faa1bc8f956ecc83800103ca0899671a9
Thisara-Chamika <thisarachamika2003@gmail.com>
2025-12-29 00:47:26 +0530

    Added the footer

--- src/pages/CoinDetail.jsx
+++ src/pages/CoinDetail.jsx
@@ -181,6 +181,9 @@ export const CoinDetail = () => {
                     </div>
                 </div>
             </div>
+            <footer className="footer">
+                <p>Data provided by CoinGecko API</p>
+            </footer>
         </div>
     )
 }
